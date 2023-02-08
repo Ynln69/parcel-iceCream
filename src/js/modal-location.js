e3f1847335539c6d6f1bc8ec9c60efdc47c4217b
Yana <yanalunina96@gmail.com>
2023-02-08 20:51:20 +0200

    створена модалка для мапи "наша локація"

--- src/js/modal-location.js
+++ src/js/modal-location.js
@@ -1,14 +1,15 @@
 (() => {
   const refs = {
-    openModalBtns: document.querySelectorAll('[data-modal-buy-now-open]'),
-    closeModalBtn: document.querySelector('[data-modal-buy-now-close]'),
-    modal: document.querySelector('[data-modal-buy-now]'),
+    openModalBtns: document.querySelectorAll('[data-location-open]'),
+    closeModalBtn: document.querySelector('[data-location-close]'),
+    modal: document.querySelector('[data-location]'),
   };
 
   refs.openModalBtns.forEach(btn => btn.addEventListener('click', toggleModal));
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
 })();
